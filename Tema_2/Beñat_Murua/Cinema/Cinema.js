1ddfb66441d0912a0b90621dc61fd80f356ee6cc
BenMurua <benat.murua594@comunidadunir.net>
2025-04-06 21:47:20 +0200

    Corrección tema 2+ tareas tema 3 (#106)
    * Añadiendo archivos Murua
    
    * Corregido Tema 2 Cinema
    
    * Subir tarea 3 Murua
    
    ---------
    
    Co-authored-by: UNIR - Software Engineering <115072043+UnirCs@users.noreply.github.com>

---
+++ Tema_2/Beñat_Murua/Cinema/Cinema.js
@@ -0,0 +1,50 @@
+const N=10; //Numero de filas y  columnas
+
+function inicializar_asientos(){
+    let id=1;
+    let asientos=[];
+    for (i=0;i<N;i++){
+        let fila=[];
+        for (j=0;j<N;j++){
+            fila.push({id:id,ocupado:false});
+            id++;
+        }
+        asientos.push(fila);
+    }
+    return asientos;
+}
+
+let asientos=inicializar_asientos();
+
+
+//Probamos a ocupar algunos de los asientos
+asientos[8][1].ocupado=true;
+asientos[9][5].ocupado=true;
+asientos[9][3].ocupado=true;
+console.log(asientos);
+
+//Funcion para seleccionar los asientos.
+function suggest(numero_asientos_solicitados){
+    let set_salida=new Set();
+    for (let i = asientos.length - 1; i >= 0; i--){
+        for (j=0; j<asientos[i].length;j++){
+            if(set_salida.size!=numero_asientos_solicitados){//Si no se han encontrado los asientos todavia, se buscan.
+                if (asientos[i][j].ocupado==false){
+                    set_salida.add(asientos[i][j].id);
+                    console.log(set_salida.size);
+                    console.log(numero_asientos_solicitados);
+                }
+                else {
+                    set_salida.clear();
+                }    
+            }
+        }
+        if(set_salida.size!=numero_asientos_solicitados){ //Si al cambiar de linea no se han encontrado asientos, se elimina datos de set_salida
+            set_salida.clear();
+        }
+    }
+    return(set_salida);
+}
+//Para probar la función.
+console.log(suggest(9));
+
